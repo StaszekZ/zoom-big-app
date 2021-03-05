48cbc95a160d94e12ee5252e7b6a2f95f820cc9d
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2021-03-05 15:57:47 +0100

    first commit and working app

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const { v4: uuidV4 } = require('uuid');
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.use('/peerjs', peerServer)
+app.get('/', (req, res)=> {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res)=> {
+  res.render('room', {roomID: req.params.room})
+})
+
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomID, userID) => {
+    socket.join(roomID);
+    socket.to(roomID).broadcast.emit('user-connected', userID);
+    
+    socket.on('message', message => {
+      io.to(roomID).emit('createMessage', message )
+    })
+  })
+  
+})
+
+
+
+server.listen(3030);
